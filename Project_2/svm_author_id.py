ae581c32f165c357f832590a2d12df920ff87580
Vishal Gaur <34275219+SwordSaintLancelot@users.noreply.github.com>
2020-04-30 13:07:16 +0300

    SVM

--- Project_2/svm_author_id.py
+++ Project_2/svm_author_id.py
@@ -13,7 +13,7 @@ from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
 from sklearn.svm import SVC
-
+from sklearn.metrics import accuracy_score
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
@@ -26,7 +26,19 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 
-model = SVC(kernel = 'linear', gamma = 10.0, C = 1)
+# features_train = features_train[:int(len(features_train)/100)]
+# labels_train = labels_train[:int(len(labels_train)/100)]
+
+model = SVC(kernel = 'rbf', C = 100)
+t0 = time()
 model.fit(features_train, labels_train)
-prediction = model.pred
+print('training time of the classifier is ', round(time()-t0,3), 'seconds')
+
+t1 = time()
+prediction = model.predict(features_test)
+print('Prediction time of the classifier is ', round(time()-t1,3), 'seconds')
+
+
+accuracy = accuracy_score(prediction, labels_test)
+print('Accuracy of the svm method is ', accuracy*100)
 
