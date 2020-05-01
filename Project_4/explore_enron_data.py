186ee5cd985f632d4c11b81ecc4e9f4388098087
Vishal Gaur <34275219+SwordSaintLancelot@users.noreply.github.com>
2020-05-01 16:53:51 +0300

    Project 4

---
+++ Project_4/explore_enron_data.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+
+""" 
+    Starter code for exploring the Enron dataset (emails + finances);
+    loads up the dataset (pickled dict of dicts).
+
+    The dataset has the form:
+    enron_data["LASTNAME FIRSTNAME MIDDLEINITIAL"] = { features_dict }
+
+    {features_dict} is a dictionary of features associated with that person.
+    You should explore features_dict as part of the mini-project,
+    but here's an example to get you started:
+
+    enron_data["SKILLING JEFFREY K"]["bonus"] = 5600000
+    
+"""
+
+import pickle
+import numpy as np
+
+enron_data = pickle.load(open(r"E:\GitHub\Udacity_Intro_ML\Project_4\final_project_dataset.pkl", "rb"))
+
+count= 0
+for name,value in enron_data.items():
+    if value['email_address'] != 'NaN':
+        count+=1
+
+
+
