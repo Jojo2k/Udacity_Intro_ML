f10d3820735b37b1b75f69894f846e068f62c54f
Vishal Gaur <34275219+SwordSaintLancelot@users.noreply.github.com>
2020-05-06 16:51:52 +0300

    Outliers

--- Project_6/outlier_cleaner.py
+++ Project_6/outlier_cleaner.py
@@ -10,16 +10,36 @@ def outlierCleaner(predictions, ages, net_worths):
         Return a list of tuples named cleaned_data where 
         each tuple is of the form (age, net_worth, error).
     """
-    
+    # prediction a list of predictions 
+    # ages is the list of ages of people
+    # net worth is the target value
+    # 90 elements in each list
+    import numpy as np
     cleaned_data = []
-    x = ()
-    min_error = 0
-    ### your code goes here
-    for i in range (len(predictions)):
-        error = predictions[i]- net_worths[i]
-        if error<min_error
+    error = np.array(net_worths-predictions)
     
-
+    for i in range(81):
+        pos = np.argmin(error)
+        tup = (ages[pos],net_worths[pos],error[pos])
+        cleaned_data.append(tup)
+        error = np.delete(error, pos)
+    # for iter_num in range(len(error)-1,0,-1):
+    #     for idx in range(iter_num):
+    #         if error[idx]>error[idx+1]:
+    #             temp1 = error[idx]
+    #             error[idx] = error[idx+1]
+    #             error[idx+1] = temp1
+                
+    #             temp3 = ages[idx]
+    #             ages[idx] = ages[idx+1]
+    #             ages[idx+1] = temp3
+                
+    #             temp3 = net_worths[idx]
+    #             net_worths[idx] = net_worths[idx+1]
+    #             net_worths[idx+1] = temp3
+    
+    # for i in range(81):
+    #     tup =(ages[:i],net_worths[:i],error[:i]) 
+    #     cleaned_data.append(tup)
     
     return cleaned_data
-
