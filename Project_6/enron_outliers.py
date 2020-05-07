88db39aaa0a3d1a2dbd45af7ed5f7c63a9ad5dfd
Vishal Gaur <34275219+SwordSaintLancelot@users.noreply.github.com>
2020-05-07 13:38:07 +0300

    outliers

--- Project_6/enron_outliers.py
+++ Project_6/enron_outliers.py
@@ -8,12 +8,14 @@ from feature_format import featureFormat, targetFeatureSplit
 
 
 ### read in data dictionary, convert to numpy array
-data_dict = pickle.load( open("../Project_6/final_project_dataset.pkl", "r") )
+data_dict = pickle.load( open("../Project_6/final_project_dataset.pkl", "r+b") )
 features = ["salary", "bonus"]
+data_dict.pop('TOTAL')
 data = featureFormat(data_dict, features)
 
 
 ### your code below
+print(data.max())
 for points in data:
     salary = points[0]
     bonus = points[1]
@@ -22,3 +24,12 @@ for points in data:
 matplotlib.pyplot.xlabel('Salary')
 matplotlib.pyplot.ylabel('Bonus')
 matplotlib.pyplot.show()
+
+outlier = []
+for key, value in data_dict.items():
+    if value['salary'] == 'NaN' or value['bonus'] == "NaN":
+        continue
+    elif value['salary']> 1000000 and value['bonus']>5000000:
+        print(key)
+        
+    
